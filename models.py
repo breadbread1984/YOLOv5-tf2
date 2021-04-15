4a251d5d359f365e17982945aaba26c5f7398c8f
breadbread1984 <breadbread1984@163.com>
2021-04-15 18:11:03 +0800

    update

--- models.py
+++ models.py
@@ -28,8 +28,9 @@ def ConvBlockLeakyReLU(input_shape, filters, kernel_size, strides = (1, 1), acti
 def ResBlock(input_shape, filters, blocks, output_filters, output_kernel, downsample = False):
 
   inputs = tf.keras.Input(shape = input_shape);
-  results = ConvBlockMish(inputs.shape[1:], filters // 2, (1, 1))(inputs);
-  short = ConvBlockMish(inputs.shape[1:], filters // 2, (1, 1))(inputs);
+  results = tf.keras.layers.ZeroPadding2D(padding = ((1,0),(1,0)))(inputs);
+  results = ConvBlockMish(inputs.shape[1:], filters // 2, (1, 1))(results);
+  short = ConvBlockMish(inputs.shape[1:], filters // 2, (1, 1))(results);
   for i in range(blocks):
     block_short = results;
     results = ConvBlockMish(results.shape[1:], filters // 2, (1, 1))(results);
@@ -44,10 +45,10 @@ def Body(input_shape):
 
   inputs = tf.keras.Input(shape = input_shape);
   cb = ConvBlockMish(inputs.shape[1:], 32, (3, 3))(inputs); # cb.shape = (batch, h, w, 32)
-  cb = ConvBlockMish(cb.shape[1:], 64, (1, 1))(cb); # cb.shape = (batch, h, w, 64)
-  rb1 = ResBlock(cb.shape[1:], filters = 64, blocks = 1, output_filters = 128, output_kernel = (3, 3))(cb); # rb1.shape = (batch, h, w, 128)
-  rb2 = ResBlock(rb1.shape[1:], filters = 128, blocks = 3, output_filters = 256, output_kernel = (3, 3))(rb1); # rb2.shape = (batch, h, w, 256)
-  rb3 = ResBlock(rb2.shape[1:], filters = 256, blocks = 3, output_filters = 512, output_kernel = (3, 3))(rb2); # rb3.shape = (batch, h, w, 512)
+  cb = ConvBlockMish(cb.shape[1:], 64, (1, 1), strides = (2, 2))(cb); # cb.shape = (batch, h / 2, w / 2, 64)
+  rb1 = ResBlock(cb.shape[1:], filters = 64, blocks = 1, output_filters = 128, output_kernel = (3, 3), downsample = True)(cb); # rb1.shape = (batch, h / 4, w / 4, 128)
+  rb2 = ResBlock(rb1.shape[1:], filters = 128, blocks = 3, output_filters = 256, output_kernel = (3, 3), downsample = True)(rb1); # rb2.shape = (batch, h / 8, w / 8, 256)
+  rb3 = ResBlock(rb2.shape[1:], filters = 256, blocks = 3, output_filters = 512, output_kernel = (3, 3), downsample = True)(rb2); # rb3.shape = (batch, h / 16, w / 16, 512)
   return tf.keras.Model(inputs = inputs, outputs = (rb1, rb2, rb3));
 
 def YOLOv5(input_shape = (608, 608, 3), class_num = 80, anchor_num = 3):
@@ -58,7 +59,7 @@ def YOLOv5(input_shape = (608, 608, 3), class_num = 80, anchor_num = 3):
   pool2 = tf.keras.layers.MaxPool2D(pool_size = (9, 9), strides = (1, 1), padding = 'same')(large);
   pool3 = tf.keras.layers.MaxPool2D(pool_size = (5, 5), strides = (1, 1), padding = 'same')(large);
   results = tf.keras.layers.Concatenate(axis = -1)([pool1, pool2, pool3, large]);
-  results = ConvBlockMish(results.shape[1:], 512, (1, 1))(results);
+  results = ConvBlockMish(results.shape[1:], 512, (1, 1), (2, 2))(results);
   large_feature = ResBlock(results.shape[1:], filters = 512, blocks = 1, output_filters = 256, output_kernel = (1, 1))(results);
   results = tf.keras.layers.UpSampling2D(2, interpolation = 'nearest')(large_feature);
   raw_middle_feature = ConvBlockLeakyReLU(results.shape[1:], 256, (1, 1))(results);
